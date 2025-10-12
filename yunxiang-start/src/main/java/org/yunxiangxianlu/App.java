2902717b07be55d2c134707d44a55c80e9f4afd1
xiangqin.wen <758087477@qq.com>
2025-10-12 17:03:59 +0800

    图片上传配置

--- yunxiang-start/src/main/java/org/yunxiangxianlu/App.java
+++ yunxiang-start/src/main/java/org/yunxiangxianlu/App.java
@@ -3,9 +3,6 @@ package org.yunxiangxianlu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @MapperScan("org.yunxiangxianlu.dal.dao")
@@ -15,15 +12,4 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addResourceHandlers(ResourceHandlerRegistry registry) {
-                // 配置静态资源路径，使上传的图片可以通过URL访问
-                registry.addResourceHandler("/images/**")
-                        .addResourceLocations("file:./uploads/");
-            }
-        };
-    }
 }
--- yunxiang-start/src/main/java/org/yunxiangxianlu/config/WebConfig.java
+++ yunxiang-start/src/main/java/org/yunxiangxianlu/config/WebConfig.java
@@ -1,8 +1,10 @@
 package org.yunxiangxianlu.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.yunxiangxianlu.interceptor.AuthInterceptor;
 
@@ -20,7 +22,20 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns( // 排除不需要认证的路径
                         "/auth/login",
                         "/auth/register",
-                        "/auth/refresh"
+                        "/auth/refresh",
+                        "/image/images/**"
                 );
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                // 配置静态资源路径，使上传的图片可以通过URL访问
+                registry.addResourceHandler("/images/**")
+                        .addResourceLocations("file:./uploads/");
+            }
+        };
     }
 }
